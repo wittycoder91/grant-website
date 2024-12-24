e3f6cc58d13d1ce084d182e377472757b0fb915e
wittycoder <julia.kevin1149@gmail.com>
2024-12-24 02:28:19 -0800

    allow to access from other pc

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,10 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
